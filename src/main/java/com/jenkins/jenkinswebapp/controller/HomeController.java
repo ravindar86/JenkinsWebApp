ac54e27ae6ab11e5239d6e1dad3b7175f3909f92
ravindar86 <ravindar.mca@gmail.com>
2021-12-07 21:00:38 +0530

    added mvc

--- src/main/java/com/jenkins/jenkinswebapp/controller/HomeController.java
+++ src/main/java/com/jenkins/jenkinswebapp/controller/HomeController.java
@@ -1,13 +1,23 @@
 package com.jenkins.jenkinswebapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @GetMapping
+    /*@GetMapping
     public String hello(){
         return "Hello!!";
+    }*/
+
+    @GetMapping("/")
+    public String greeting() {
+        return "hello";
     }
 }
