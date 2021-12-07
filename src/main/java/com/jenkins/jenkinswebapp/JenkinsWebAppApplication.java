9138b8db90d70a1c3d37499979a8b58abafcde1a
ravindar86 <ravindar.mca@gmail.com>
2021-12-07 18:09:46 +0530

    Initial commit

---
+++ src/main/java/com/jenkins/jenkinswebapp/JenkinsWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jenkins/jenkinswebapp/JenkinsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
